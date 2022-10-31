389515e8f8a508e1563419740c3be6510930b705
Mohamed1380 <mabdalhady138@gmail.com>
2022-10-31 08:46:49 +0200

    jenkins file CD Build eks using terraform

--- aws_infra_eks/backend.tf
+++ aws_infra_eks/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "abdelhady1"
+    #bucket         = "abdelhady1"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     # dynamodb_table = "db-table"
